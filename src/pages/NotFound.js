dccc7b30b9fa3ce376502a51bdbe85dccfe99e87
Lina <linadragelante@gmail.com>
2022-07-14 12:52:33 -0700

    fixed react router

---
+++ src/pages/NotFound.js
@@ -0,0 +1,12 @@
+
+const NotFound = () => {
+    return ( 
+        <div className="not-found">
+            <h2> Sorry </h2>
+            <p>That page cannot be found</p>
+            
+        </div>
+    );
+}
+
+export default NotFound;
